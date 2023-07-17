98b9c290924e083127f7c25018d5c6e8d6192233
hfreecmu <hfreeman@andrew.cmu.edu>
2023-07-16 21:58:33 -0400

    combined octree

---
+++ include/octomap_vpp_rviz_plugin/combined_octree_display.h
@@ -0,0 +1,29 @@
+#ifndef COMBINED_OCTREE_DISPLAY_H
+#define COMBINED_OCTREE_DISPLAY_H
+
+#include "octomap_rviz_plugins/occupancy_grid_display.h"
+#include "octomap_vpp/CombinedOcTree.h"
+#include "rviz/properties/color_property.h"
+
+namespace octomap_vpp_rviz_plugin
+{
+
+class CombinedOcTreeDisplay : public octomap_rviz_plugin::OccupancyGridDisplay
+{
+Q_OBJECT
+public:
+  CombinedOcTreeDisplay();
+protected:
+  void incomingMessageCallback(const octomap_msgs::OctomapConstPtr& msg);
+  void setVoxelColor(rviz::PointCloud::Point& newPoint, octomap_vpp::CombinedOcTreeNode& node, double minZ, double maxZ);
+  ///Returns false, if the type_id (of the message) does not correspond to the template paramter
+  ///of this class, true if correct or unknown (i.e., no specialized method for that template).
+  bool checkType(std::string type_id);
+
+  rviz::ColorProperty* min_color_property_;
+  rviz::ColorProperty* max_color_property_;
+};
+
+}
+
+#endif // REGION_OCTREE_DISPLAY_H
